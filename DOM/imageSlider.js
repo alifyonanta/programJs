e7ac6e2bd63f6c9d35779ec4373f4286fe651907
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2020-10-03 09:11:48 +0700

    Create imageSlider.js

---
+++ DOM/imageSlider.js
@@ -0,0 +1,31 @@
+//JavaScript code will go here
+// by Dmitri Tchemski special for sololearn.com
+
+window.onload = function() {
+
+    var slider = {
+        _images:[
+           "http://www.sololearn.com/uploads/slider/1.jpg", 
+           "http://www.sololearn.com/uploads/slider/2.jpg", 
+           "http://www.sololearn.com/uploads/slider/3.jpg"
+        ],
+        _current:0,
+        next:function(){
+            if(++slider._current == slider._images.length) {
+                slider._current = 0;
+            }
+            document.getElementById("slider").src = slider._images[slider._current];
+        },
+        prev:function(){
+            if(--slider._current < 0){
+                slider._current = slider._images.length - 1;
+            }
+            document.getElementById("slider").src = slider._images[slider._current];
+        }
+        
+    };
+    
+    document.getElementById("next").addEventListener("click", slider.next);
+    document.getElementById("prev").addEventListener("click", slider.prev);
+
+}
