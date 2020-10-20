d465bcebf60dc173ead6a31d6f711fafdbcd86b7
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2020-10-20 15:29:08 +0700

    Create classes.js

---
+++ ES6/classes.js
@@ -0,0 +1,88 @@
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+  }
+}
+
+const square = new Rectangle(5, 5);
+const poster = new Rectangle(2, 3); 
+
+console.log(square.height);
+
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+  
+  get area() {
+    return this.calcArea();
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+const square = new Rectangle(5, 5);
+
+console.log(square.area); // 25
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static distance(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+
+    return Math.hypot(dx, dy);
+  }
+}
+
+const p1 = new Point(7, 2);
+const p2 = new Point(3, 8);
+
+console.log(Point.distance(p1, p2));
+
+//inheritance
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    console.log(this.name + ' makes a noise.');
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    console.log(this.name + ' barks.');
+  }
+}
+let dog = new Dog('Rex');
+dog.speak(); // Rex barks.
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    console.log(this.name + ' makes a noise.');
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    super.speak(); // Super
+    console.log(this.name + ' barks.');
+  }
+}
+
+let dog = new Dog('Rex');
+dog.speak();
+// Rex makes a noise.
+// Rex barks.
